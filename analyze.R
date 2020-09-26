e2ecb1eaeca60fa74089951c556bdaace0aa7aad
Jordan <jordan@Jordans-MacBook-Air.local>
2020-09-26 17:17:23 -0400

    Create additional models and pick best one

--- analyze.R
+++ analyze.R
@@ -91,26 +91,102 @@ train = subset(rhbdata, split == TRUE)
 test = subset(rhbdata, split == FALSE)
 
 #we have multiple variables so lets logistical regression it
-model1 = lm(priceusa ~ division + formatname + pages + category + lowerage + upperage, train)
+model1 = lm(priceusa ~ division + formatname + pages + category + saleyear + salemonth + lowerage + upperage, train)
 summary(model1)
 
 #based off initial testing age range does not seem to matter 
-model2 = lm(priceusa ~ division + formatname + pages + category, train)
+model2 = lm(priceusa ~ division + formatname + pages + category + saleyear + salemonth, train)
 summary(model2)
 
 #predict based on our model2
 prediction2 = predict(model2, test)
+
+#check accuracy and calculate error
 summary(prediction2)
+R2 = 1-sum((test$priceusa - prediction2)^2)/sum((test$priceusa - mean(test$priceusa))^2)
+RMSE2 = sqrt(sum(((test$priceusa - prediction2)^2)/nrow(test)))
+
 plot(test$priceusa, prediction2) #visual of our outcome vs prediction
 
 #does our model work better on smaller prices?
-train2 = train[train$priceusa <=40, ]
-test2 = test[test$priceusa <=40, ]
-model3 = lm(priceusa ~ division + formatname + pages + category, train2)
-summary(model3) #yes it does
+trainsmall = train[train$priceusa <=40, ]
+testsmall = test[test$priceusa <=40, ]
+model3 = lm(priceusa ~ division + formatname + pages + category + saleyear + salemonth, trainsmall)
+summary(model3) 
 
 #predict based on our model3
-prediction3 = predict(model3, test2)
+prediction3 = predict(model3, testsmall)
 summary(prediction3)
-plot(test2$priceusa, prediction3)
+R3 = 1-sum((testsmall$priceusa - prediction3)^2)/sum((testsmall$priceusa - mean(testsmall$priceusa))^2)
+RMSE3 = sqrt(sum(((testsmall$priceusa - prediction3)^2)/nrow(testsmall)))
+
+#yes it does, but does it on the larger set too?
+prediction3l = predict(model3, test)
+summary(prediction3l)
+R3l = 1-sum((test$priceusa - prediction3l)^2)/sum((test$priceusa - mean(test$priceusa))^2)
+RMSE3l = sqrt(sum(((test$priceusa - prediction3l)^2)/nrow(test))) 
+#its very similar, although our model 2 does slightly better
+
+#how about the first model for everything on the smaller set
+prediction2s = predict(model2, testsmall)
+summary(prediction2s)
+R2s = 1-sum((testsmall$priceusa - prediction2s)^2)/sum((testsmall$priceusa - mean(testsmall$priceusa))^2)
+RMSE2s = sqrt(sum(((testsmall$priceusa - prediction2s)^2)/nrow(testsmall))) 
+#its very similar, although our model 3 does slightly better
+
+(RMSE2+RMSE2s)/2
+(RMSE3+RMSE3l)/2 
+#very slight win on average for model based on smaller prices
+#since this is 99% of our data lets plot it and see where it tends to be off
+plot(prediction3l, test$priceusa, type = 'p', main = "Model 3 Prediction", xlab = "Predicted Price", ylab = "Actual Price")
+abline(a=0, b=1, col = 'red')
+# usually off on the higher price items (as expected)
+# better on the small items by .18 (cents)
+# and worse on large items by .175 (cents)
+# since we would rather be closer on the 99% lets use model3 (although I don't think it really matters)
+
+
+#plots and data for our analysis write up
+
+#separating price into bins to plot count
+rhbdata$pricegroup = cut(rhbdata$priceusa, 
+                  breaks=c(0,5,10,15,20,25,30,100), 
+                  include.lowest=TRUE, 
+                  right=FALSE, 
+                  labels=c("$0-5","$5-10", "$10-15", "$15-20", "$20-25", "$25-30", "$30+"))
+ggplot(data = as_tibble(rhbdata$pricegroup), mapping = aes(x=value)) + 
+  geom_bar(fill="light green", color="white", alpha=0.7) + 
+  stat_count(geom="text", aes(label=sprintf("%.2f%%",..count../length(rhbdata$pricegroup)*100)), vjust=-0.5) +
+  labs(title = 'Number of Books by Price Range', x = 'Price', y = "Number of Books") +
+  expand_limits(y = c(0, 50000)) +
+  theme_bw()
+
+summary(rhbdata)
+sd(rhbdata$priceusa)
+13.28/8.605371 # mean/sd to get CV
+
+#summarizing categorical data
+datagroup = rhbdata %>%
+  group_by(category)%>% 
+  summarise(Count=length(priceusa), Mean=mean(priceusa), Median=median(priceusa), Min=min(priceusa), Max=max(priceusa))
+
+#decided not to use but useful for future perhaps
+ggplot(data = rhbdata, aes(x = category, y = priceusa))  +
+  geom_jitter(width = .2, color="light green", alpha=.4) +
+  geom_boxplot(outlier.shape = NA, fill = 'grey', alpha = .15) +
+  labs(title = 'Price of Books by Category', x = 'Category', y = "Price of Books") +
+  theme_bw()
+
+#plotting correlation for numerical data
+datagroup = rhbdata %>%
+  group_by(saleyear)%>% 
+  summarise(mean=mean(priceusa), median=median(priceusa))
+datagroup = melt(datagroup, id = "saleyear")
+ggplot(data = datagroup, aes(x = saleyear, y = value,  color = variable)) + 
+  geom_point(aes(group = variable)) +
+  geom_line(aes(group = variable)) +
+  scale_color_manual(values = c('mean' = 'light green','median' = 'light blue')) +
+  labs(title = 'Year Correlation', x = 'Year', y = "Price of Books", color = '') +
+  theme_bw()
+
 
